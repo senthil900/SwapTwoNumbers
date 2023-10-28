97a4a64999ffaf8d224338bd37117b24ac8802a8
senthil900 <110320976+senthil900@users.noreply.github.com>
2023-10-28 16:05:22 +0530

    Add files via upload

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,14 @@
+package basictraining;
+
+public class SwapTwoNumbers {
+	public static void main(String args[]) {
+		int a=15;
+		int b=10;
+		int sum=a+b;
+		a=sum-a;
+		b=sum-b;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
